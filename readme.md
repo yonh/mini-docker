c483941c57d3a35940b47f19384e4879adb1660d
Yonh Lai <azssjli@163.com>
2018-09-30 17:55:24 +0800

    add user namespace example

--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 # mini-docker
 
+## 实验环境
+* Ubuntu 16.04 - 4.4.0-135-generic
+* Go 1.6.2
+* Docker 17.03.2-ce
+  * Client: API version: 1.27
+  * Server: API version: 1.27 (minimum version 1.12)
+
 
 ## 1. 启动环境
 vagrant up
-
-
-
 
---
+++ src/example/user.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	// 指定被 fork 出来的新进程内的初始命令
+	cmd := exec.Command("sh")
+
+	// 指定进程的namespace参数
+	// 设置启动用户用户组mapping参数
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:  syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1,},},
+		GidMappings: []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1,},},
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
