8973b55dded81d4a758681f5b919977b28af7b57
Yonh Lai <azssjli@163.com>
2018-10-28 23:00:37 +0800

    update app

--- src/mini-docker/main.go
+++ src/mini-docker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
 	"log"
 	"os"
@@ -13,6 +14,10 @@ func main() {
 	app.Usage = "mini-docker is a simple container runtime implementation."
 	app.Version = "0.0.1"
 
+	app.Commands = []cli.Command{
+		initCommand,
+		runCommand,
+	}
 
 	app.Before = func(context *cli.Context) error {
 		log.SetOutput(os.Stdout)
@@ -20,7 +25,31 @@ func main() {
 		return nil
 	}
 
+
+
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+var runCommand = cli.Command{
+	Name: "run",
+	Usage: "create a container with namespace and cgroup, docker run -it [command]",
+	Flags: []cli.Flag {
+		cli.BoolFlag{
+			Name: "it",
+			Usage: "enable tty",
+		},
+	},
+	Action: func(context *cli.Context) error {
+		fmt.Println("hello, I'm run command.")
+		return nil
+	},
+}
+var initCommand = cli.Command{
+	Name: "init",
+	Usage: "init container process run user's process in container. Don't call it outside.",
+	Action: func(context *cli.Context) error {
+		return nil
+	},
 }
