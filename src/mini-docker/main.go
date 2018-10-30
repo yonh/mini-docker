0f11cc13bc96feed724284fc4c24dcbccdbb13d0
Yonh Lai <azssjli@163.com>
2018-10-30 23:47:22 +0800

    update note

--- src/mini-docker/main.go
+++ src/mini-docker/main.go
@@ -95,6 +95,7 @@ func NewParentProcess(tty bool, command string) *exec.Cmd {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
 			syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
 	}
+	// 如果用户指定了 -it 参数, 就把当前进程的输入输出导入到标准输入输出上
 	if tty {
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
