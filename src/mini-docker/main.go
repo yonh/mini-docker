935ac4effb83135bd11d2c70658cb6fe3e4bfbe0
Yonh Lai <azssjli@163.com>
2018-11-01 01:14:02 +0800

    更新initCommand解释

--- src/mini-docker/main.go
+++ src/mini-docker/main.go
@@ -60,33 +60,23 @@ var initCommand = cli.Command{
 
 		log.Println("init...")
 		cmd := context.Args().Get(0)
-		log.Printf("command %s\n", cmd)
+		log.Printf("initCommand %s\n", cmd)
 		err := RunContainerInitProcess(cmd, nil)
 		return err
 	},
 }
 
-func RunContainerInitProcess(command string, args []string) error {
-	log.Printf("command %s \n", command)
-
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	argv := []string{command}
-	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
-		log.Fatal(err.Error())
-	}
-	return nil
-}
 
 /**
  * 这里会启动一个子进程，子进程传入init参数，使得子进程调用init实现初始化的调用
  */
 func Run(tty bool, command string) {
-
+	// NewParentProcess初始化进程
 	parent := NewParentProcess(tty, command)
 	if err := parent.Start(); err != nil {
 		log.Fatal(err)
 	}
+
 	parent.Wait()
 	os.Exit(-1)
 }
@@ -112,3 +102,25 @@ func NewParentProcess(tty bool, command string) *exec.Cmd {
 	return cmd
 }
 
+/**
+ * 这里的代码是由容器内这个进程执行的
+ * 这里首先mount proc 文件系统
+ */
+func RunContainerInitProcess(command string, args []string) error {
+	log.Printf("RunContainerInitProcess: %s \n", command)
+
+	// MS_NOEXEC 在本文件系统下不允许运行其他程序
+	// MS_NOSUID 不允许set uid 或 set gid
+	// MS_NODEV  这个是自linux 2.4以来，所有mount都会默认设置的一个参数
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	argv := []string{command}
+
+	// 这里的syscall.Exec比较重要，按照我们目前的代码看来，容器执行后执行的第一个进程并不是我们指定的进程
+	// 那我们通过ps查看到的pid=1的进程不会是我们指定的进程，通过syscall.Exec 这个方法就可以解决这个问题
+	// 它会覆盖当前进程的镜像，数据，和堆栈信息，包括PID，使得我们可以替换掉本身的init进程。
+	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
+		log.Fatal(err.Error())
+	}
+	return nil
+}
